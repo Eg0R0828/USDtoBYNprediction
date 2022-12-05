97757b750d320cd624bad4c5f6c4fbaf0e79fa0c
Eg0R0828 <egorhh357@gmail.com>
2022-12-05 16:09:15 +0300

    add new model, refactor project

---
+++ OutputValuePrediction/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 *-*
+
+
+from createNNmodel import create_nn_model
+from testNNmodel import test_saved_nn_model
+
+
+# Time period (for one "candle")
+n = 7
+
+if input('Do You want to create a new NN-model? (Yes / other word) ') == 'Yes':
+    create_nn_model(n)
+if input('Do You want to test saved NN-model? (Yes / other word) ') == 'Yes':
+    test_saved_nn_model(n, input('Enter a test data file path: '))
